a6c840fe7598b8c71c512cc36da219f7a20cb70f
pedrodel26 <pedromodesto26@gmail.com>
2024-05-07 09:53:13 -0300

    Initial Commit

---
+++ GitHub Exercise1/GitHub_Exercise1App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_Exercise1App.swift
+//  GitHub Exercise1
+//
+//  Created by Pedro Delmondes  on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_Exercise1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
