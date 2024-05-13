c3d3bca5a055fc8be5f2b8c33dc695c512b898dc
pedrodel26 <pedromodesto26@gmail.com>
2024-05-13 00:15:08 -0300

    New File GitTestr

---
+++ GitHub Exercise1/GitTest.swift
@@ -0,0 +1,18 @@
+//
+//  GitTest.swift
+//  GitHub Exercise1
+//
+//  Created by Pedro Delmondes  on 11/05/2024.
+//
+
+import SwiftUI
+
+struct GitTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GitTest()
+}
